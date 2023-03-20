b9f559db34b54ec8fa49b5b61af10fcbe6b02cba
yisuscoin <r.alfaro1882@gmail.com>
2023-03-19 19:46:19 -0500

    dando privacidad url y clave privada con .env

--- development/hardhat/hardhat.config.js
+++ development/hardhat/hardhat.config.js
@@ -5,10 +5,8 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     goerli: {
-      url:'https://eth-goerli.g.alchemy.com/v2/fXJSVcg4rZ0B4FAH3OoEIyI0p3xQX9XS',
-      accounts: [
-        '10c6f93748d9adecd61510850ed7a87a62a1a86bf66ae181b97ea536d07525c2'
-      ]
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.GOERLI_ACCOUNTS],
     }
   }
 };
